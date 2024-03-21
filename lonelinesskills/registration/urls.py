917c14c9d5bc33289e1bbc34a800784b71d2824c
fanazz <pskirma@gmail.com>
2024-03-21 23:46:57 +0200

    taisymas

--- lonelinesskills/registration/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # path('', views.signup, name='index'),
-    # path('signup', views.signup, name='signup'),
-    path('volunteers', views.volunteer_list, name='volunteer_list'),
-    path('lonelyhuman', views.lonely_human_list, name='lonely_human_list'),
-]
